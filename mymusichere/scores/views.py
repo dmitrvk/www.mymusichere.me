b874c18754c573d0609243fbe51fab14d69e5b3e
Dmitry K <dmitrykalyukov@gmail.com>
2020-03-25 11:02:52 +0200

    Initial commit

---
+++ mymusichere/scores/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse, Http404
+
+from .models import Score
+
+def index(request):
+    scores_count = len(Score.objects.all())
+    context = {
+        'scores_count': scores_count
+    }
+    return render(request, 'scores/index.html', context)
+
+def score_view(request, title):
+    try:
+        score = Score.objects.get(title=title)
+        context = {
+                'score_title': score.title,
+                'score_path_to_file': score.path_to_file
+        }
+        response = render(request, 'scores/score.html', context)
+        return response
+    except Score.DoesNotExist:
+        raise Http404("Score '%s' not found" % title)
+
