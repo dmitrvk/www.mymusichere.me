d37e65b7ac776c62c9db25ed7e2daa1df29b7c09
Dmitry K <dmitrykalyukov@gmail.com>
2020-04-01 12:28:01 +0200

    Serve static files during development

--- mymusichere/urls.py
+++ mymusichere/urls.py
@@ -10,3 +10,13 @@ urlpatterns = [
     path('hooks/', include('hooks.urls')),
     path('', include('scores.urls'))
 ]
+
+
+# Serve static files during development
+
+from django.conf import settings
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
