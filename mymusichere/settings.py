8b7eada09e4696c777f33d3c8bd52242681c0af6
Dmitry K <dmitrykalyukov@gmail.com>
2020-04-23 17:29:42 +0200

    Create custom error pages

--- mymusichere/settings.py
+++ mymusichere/settings.py
@@ -38,7 +38,7 @@ ROOT_URLCONF = 'mymusichere.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'mymusichere', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
