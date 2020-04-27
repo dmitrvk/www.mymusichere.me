e307c4035b09bb893672ac230a344761b5df82f6
Dmitry K <dmitrykalyukov@gmail.com>
2020-04-27 12:58:35 +0200

    Setup basic logging

--- deploy-scores.py
+++ deploy-scores.py
@@ -21,13 +21,18 @@ MYMUSICHERE_REMOTE = os.environ['MYMUSICHERE_REMOTE']
 
 TOKEN = 'Token %s' % os.environ['DEPLOY_TOKEN']
 
+LOGGING_FORMAT = '%(levelname)s:%(asctime)s:%(module)s:%(message)s'
+
+logger = logging.getLogger(__name__)
 
 def main():
-    logfile = os.path.join(BASE_DIR, 'deploy-scores.log')
-    logging.basicConfig(filename=logfile, level=logging.INFO)
+    logger.setLevel(logging.INFO)
+    fileHandler = logging.FileHandler(os.path.join(BASE_DIR, 'mymusichere.log'))
+    fileHandler.setLevel(logging.INFO)
+    fileHandler.setFormatter(logging.Formatter(LOGGING_FORMAT))
+    logger.addHandler(fileHandler)
 
-    time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    logging.info('%s:Check for update' % time)
+    logger.info('Check for update')
 
     try:
         repo = Repo(MYMUSICHERE_REPO_DIR)
@@ -35,29 +40,29 @@ def main():
         if len(sys.argv) > 1 and sys.argv[1] == '--force':
             deploy_scores(repo)
         elif updates_available(repo):
-            logging.info('Changes detected')
+            logger.info('Changes detected')
             deploy_scores(repo)
         else:
-            logging.info('No changes detected')
+            logger.info('No changes detected')
 
     except InvalidGitRepositoryError:
-        logging.warning('Invalid git repo at %s. Cloning...' % MYMUSICHERE_REPO_DIR)
+        logger.warning('Invalid git repo at %s. Cloning...' % MYMUSICHERE_REPO_DIR)
         rmtree(MYMUSICHERE_REPO_DIR)
         repo = Repo.clone_from(MYMUSICHERE_REMOTE, MYMUSICHERE_REPO_DIR, branch='master')
         deploy_scores(repo)
 
     except NoSuchPathError:
-        logging.warning('No such path %s. Cloning remote repo...' % MYMUSICHERE_REPO_DIR)
+        logger.warning('No such path %s. Cloning remote repo...' % MYMUSICHERE_REPO_DIR)
         repo = Repo.clone_from(MYMUSICHERE_REMOTE, MYMUSICHERE_REPO_DIR, branch='master')
         deploy_scores(repo)
 
     except Exception as e:
-        logging.error('Error: %s' % e)
+        logger.error('Error: %s' % e)
         sys.exit(1)
 
 
 def updates_available(repo):
-    logging.info('git fetch...')
+    logger.info('git fetch...')
     fetchinfo = repo.remotes.origin.fetch()
     return repo.heads[0].commit != fetchinfo[0].commit
 
@@ -65,16 +70,16 @@ def updates_available(repo):
 def deploy_scores(repo):
     clean_repo(repo)
 
-    logging.info('git pull...')
+    logger.info('git pull...')
     repo.remotes.origin.pull()
 
-    logging.info('make')
+    logger.info('make')
     completed_make = subprocess.run(['make'], cwd=MYMUSICHERE_REPO_DIR)
 
     if completed_make.returncode == 0:
-        logging.info('Scores built successfully')
+        logger.info('Scores built successfully')
     else:
-        logging.error('Scores build failed')
+        logger.error('Scores build failed')
         sys.exit(1)
 
     response = requests.post(
@@ -84,29 +89,29 @@ def deploy_scores(repo):
     )
 
     if response.status_code == 200:
-        logging.info(response.content)
+        logger.info(response.content)
 
-        logging.info('make static')
+        logger.info('make static')
         args = ['make', 'static']
         completed_make_static = subprocess.run(args, cwd=BASE_DIR)
 
         if completed_make_static.returncode == 0:
-            logging.info('Statifiles updated')
+            logger.info('Statifiles updated')
         else:
-            logging.error('Failed to update static files')
+            logger.error('Failed to update static files')
             sys.exit(1)
 
     else:
-        logging.error('Got unexpected status code %d: %s' % (response.status_code, response.content))
+        logger.error('Got unexpected status code %d: %s' % (response.status_code, response.content))
 
 
 def clean_repo(repo):
-    logging.info('Clean repo')
+    logger.info('Clean repo')
 
     subprocess.run(['make', 'clean'], cwd=MYMUSICHERE_REPO_DIR)
 
     if repo.is_dirty():
-        logging.info('Repo is dirty. All changes will be removed.')
+        logger.info('Repo is dirty. All changes will be removed.')
         repo.git.reset('--hard')
 
 
