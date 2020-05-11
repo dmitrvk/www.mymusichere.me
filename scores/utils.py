a45e2cc5e19ffdf4bd09063ffcffce3e75b12e98
Dmitry K <dmitrykalyukov@gmail.com>
2020-05-11 17:26:09 +0200

    Create LilyPond header parser

---
+++ scores/utils.py
@@ -0,0 +1,23 @@
+from operator import methodcaller
+
+def parse_lilypond_header(filename: str) -> dict:
+    """Parse header in LilyPond file and return dict with header fields."""
+    with open(filename) as f:
+        header = dict()
+        reading_header = False
+        for line in f:
+            line = line.strip()
+            if reading_header:
+                if '}' in line:
+                    reading_header = False
+                    break
+                elif '=' in line:
+                    field, value = map(methodcaller('strip'), line.split('='))
+                    if len(field) > 0 and len(value) > 0:
+                        if '"' in value:
+                            value = value.strip('"')
+                        header[field.lower()] = value
+            elif '\header' in line:
+                reading_header = True
+        return header
+
