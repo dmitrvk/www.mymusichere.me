54f70f1f9c289a5c396e989d97e6e807d148c3b3
Dmitry K <dmitrykalyukov@gmail.com>
2020-06-06 21:23:35 +0200

    Install debug toolbar

--- mymusichere/urls.py
+++ mymusichere/urls.py
@@ -16,5 +16,9 @@ urlpatterns = [
 # Serve static files during development
 
 if settings.DEBUG:
+    import debug_toolbar
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
